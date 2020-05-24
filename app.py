0a4388ff23d4b8b6c2640955b2095727012ccc44
Francismar Palma <trollzer64@gmail.com>
2020-05-23 22:51:06 -0300

    Working on raspberry

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import socketio
+import RPi.GPIO as GPIO
+import time
+import numpy as np
+from decouple import config
+
+## Definição de posição do servomotor
+minPos = 2.5                # Define a posição de fechado
+maxPos = 12.5               # Define a posição de aberto
+
+servoPIN = 17
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(servoPIN, GPIO.OUT)
+
+p = GPIO.PWM(servoPIN, 50)  # GPIO 17 para PWM de 50Hz
+p.start(2.5)                # Inicialização
+opening = False             # Define janela inicialmente fechada
 
 sio = socketio.Client()
 
@@ -6,15 +22,31 @@ sio = socketio.Client()
 def connect():
     print('connection established')
 
-@sio.on('allTweet')
+latchDouble = False     # Corrigir duplicata de dados
+@sio.on('tweet')
 def on_message(data):
-    print('I received a message!')
-    print(data)
-
-@sio.event
-def my_message(data):
-    print('message received with ', data)
-    sio.emit('my response', {'response': 'my response'})
+    global latchDouble
+    if(latchDouble):
+        print('Tweet received: ', data['tweet']['text'])
+        print("expected user: @", data['user'])
+        print("received user: @", data['tweet']['user']['screen_name'])
+        # Verificação do usuário
+        if(data['user'] == data['tweet']['user']['screen_name']):
+            # Latch abertura
+            global opening 
+            opening = not(opening)
+            message = "Opening" if opening else "Closing"
+            print(message, " Window")
+            # Define trajeto do PWM
+            traj = np.linspace(minPos, maxPos, 10) if opening else np.linspace(maxPos, minPos, 10)
+            # Realiza o trajeto
+            for i in traj:
+                p.ChangeDutyCycle(i)
+                time.sleep(0.5)
+        else:
+            print('invalid user')
+    latchDouble = not(latchDouble)
+    print('=======================')
 
 @sio.event
 def disconnect():
